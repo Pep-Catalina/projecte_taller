3d94a35ddd2c0bc50aef80c2431ac91d25f23b62
Pep-Catalina <146848634+Pep-Catalina@users.noreply.github.com>
2025-04-28 12:49:15 +0200

    a

--- db_schema.sql
+++ db_schema.sql
@@ -1,25 +1,51 @@
+CREATE DATABASE centre_medic;
+USE centre_medic;
 
-CREATE TABLE clients (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(100),
-    telefon VARCHAR(20),
+-- CREACIÓ DE TAULES:
+CREATE TABLE pacients (
+    id INT UNSIGNED,
+    nom VARCHAR(25),
+    cognom VARCHAR(25),
+    telefon VARCHAR(15),
     correu VARCHAR(100)
 );
 
-CREATE TABLE vehicles (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    client_id INT,
-    matricula VARCHAR(20),
-    model VARCHAR(50),
-    any_fabricacio INT,
-    quilometres INT,
-    FOREIGN KEY (client_id) REFERENCES clients(id)
+CREATE TABLE especialitats (
+    id INT UNSIGNED,
+    nom_especialitat VARCHAR(30),
+    descripcio VARCHAR(255)
 );
 
-CREATE TABLE cites (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    vehicle_id INT,
-    data_cita DATE,
-    servei_sollicitat VARCHAR(100),
-    FOREIGN KEY (vehicle_id) REFERENCES vehicles(id)
+CREATE TABLE visites (
+    id INT UNSIGNED,
+    pacient_id INT UNSIGNED,
+    especialitat_id INT UNSIGNED,
+    data_visita DATE,
+    motiu_visita VARCHAR(255)
 );
+
+-- PRIMARY KEYS
+ALTER TABLE pacients
+    ADD CONSTRAINT pk_pacients PRIMARY KEY (id);
+ALTER TABLE pacients    
+    MODIFY COLUMN id INT UNSIGNED AUTO_INCREMENT NOT NULL;
+
+ALTER TABLE especialitats
+    ADD CONSTRAINT pk_especialitats PRIMARY KEY (id);
+ALTER TABLE especialitats
+    MODIFY COLUMN id INT UNSIGNED;
+    
+ALTER TABLE visites
+    ADD CONSTRAINT pk_visites PRIMARY KEY (id);
+ALTER TABLE visites
+	MODIFY COLUMN id INT UNSIGNED AUTO_INCREMENT NOT NULL;
+
+ALTER TABLE visites
+    ADD CONSTRAINT fk_pacient FOREIGN KEY (pacient_id) 
+    REFERENCES pacients(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+    ADD CONSTRAINT fk_especialitat FOREIGN KEY (especialitat_id) 
+    REFERENCES especialitats(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE;
