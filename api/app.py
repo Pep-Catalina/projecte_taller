a905e6d287a22e88155b2906732a1eb8a7b86283
crimill <161335647+crimill@users.noreply.github.com>
2025-05-05 12:45:50 +0200

    xde

--- api/app.py
+++ api/app.py
@@ -6,31 +6,47 @@ app = Flask(__name__)
 
 def connect_db():
     return mysql.connector.connect(
-        host="db",
+        host="localhost",
         user="root",
-        password="example",
-        database="taller"
+        password="Educem00.",
+        database="centre_medic"
     )
 
-@app.route('/vehicles', methods=['GET'])
-def get_vehicles():
+@app.route('/pacients', methods=['GET'])
+def get_pacients():
     db = connect_db()
     cursor = db.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM vehicles")
-    vehicles = cursor.fetchall()
+    cursor.execute("SELECT * FROM pacients")
+    pacients = cursor.fetchall()
     db.close()
-    return jsonify(vehicles)
+    return jsonify(pacients)
 
-@app.route('/appointments', methods=['POST'])
-def create_appointment():
+
+@app.route('/pacients', methods=['POST'])
+def create_pacient():
     data = request.get_json()
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("INSERT INTO cites (vehicle_id, data_cita, servei_sollicitat) VALUES (%s, %s, %s)", 
-                   (data['vehicle_id'], data['data_cita'], data['servei_sollicitat']))
+
+    # Inserir pacient
+    cursor.execute("""
+        INSERT INTO pacients (nom, cognom, telefon, correu)
+        VALUES (%s, %s, %s, %s)
+    """, (data['nom'], data['cognom'], data['telefon'], data['correu']))
     db.commit()
+
+    # Obtenir l'ID del pacient recentment creat
+    pacient_id = cursor.lastrowid
+
+    # Crear la visita
+    cursor.execute("""
+        INSERT INTO visites (pacient_id, data_visita, motiu_visita)
+        VALUES (%s, %s, %s)
+    """, (pacient_id, data['data_cita'], data['motiu']))
+    db.commit()
+
     db.close()
-    return jsonify({'message': 'Cita creada'}), 201
+    return jsonify({'message': 'Pacient i visita creats correctament'}), 201
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
