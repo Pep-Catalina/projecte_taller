8d3592e158f17e5c9fef58be5dce9a2908126c99
Pep-Catalina <146848634+Pep-Catalina@users.noreply.github.com>
2025-05-05 13:04:35 +0200

    q

---
+++ docker/mysql/init.sql
@@ -0,0 +1,59 @@
+CREATE DATABASE centre_medic;
+USE centre_medic;
+
+-- CREACIÓ DE TAULES:
+CREATE TABLE pacients (
+    id INT UNSIGNED,
+    nom VARCHAR(25),
+    cognom VARCHAR(25),
+    telefon VARCHAR(15),
+    correu VARCHAR(100)
+);
+
+CREATE TABLE especialitats (
+    id INT UNSIGNED,
+    nom_especialitat VARCHAR(30),
+    descripcio VARCHAR(255)
+);
+
+CREATE TABLE visites (
+    id INT UNSIGNED,
+    pacient_id INT UNSIGNED,
+    especialitat_id INT UNSIGNED,
+    data_visita DATE,
+    motiu_visita VARCHAR(255)
+);
+
+-- PRIMARY KEYS
+ALTER TABLE pacients
+    ADD CONSTRAINT pk_pacients PRIMARY KEY (id);
+ALTER TABLE pacients    
+    MODIFY COLUMN id INT UNSIGNED AUTO_INCREMENT NOT NULL;
+
+ALTER TABLE especialitats
+    ADD CONSTRAINT pk_especialitats PRIMARY KEY (id);
+ALTER TABLE especialitats
+    MODIFY COLUMN id INT UNSIGNED;
+    
+ALTER TABLE visites
+    ADD CONSTRAINT pk_visites PRIMARY KEY (id);
+ALTER TABLE visites
+	MODIFY COLUMN id INT UNSIGNED AUTO_INCREMENT NOT NULL;
+
+ALTER TABLE visites
+    ADD CONSTRAINT fk_pacient FOREIGN KEY (pacient_id) 
+    REFERENCES pacients(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+    ADD CONSTRAINT fk_especialitat FOREIGN KEY (especialitat_id) 
+    REFERENCES especialitats(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE;
+
+
+
+-- USUARI ADMIN
+
+CREATE USER 'admin'@'%' IDENTIFIED BY 'Educem00.';
+GRANT ALL PRIVILEGES ON centre_medic.* TO 'admin'@'%';
+FLUSH PRIVILEGES;
--- docker/readme.txt
+++ docker/readme.txt
@@ -1,8 +1,8 @@
-docker image build -t nginx-digitalitzacio nginx/.
+docker image build -t nginx-digitalitzacio docker/nginx/.
 
-docker image build -t mariadb-digitalitzacio mysql/.
+docker image build -t mariadb-digitalitzacio docker/mysql/.
 
-docker network create --driver bridge --gateway 192.168.1.254 --subnet 182.168.1.0/24 --attachable digi-network
+docker network create --driver bridge --gateway 192.168.1.254 --subnet 192.168.1.0/24 --attachable digi-network
 
 docker container run -d nginx-digitalitzacio --dns 192.168.1.1 --dns-search centremedic.cat --network digi-network --ip 192.168.1.1 -p 8080:80 -p 4433:443 --hostname srv-centremedic nginx-digitalitzacio
 
